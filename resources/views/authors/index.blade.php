0bc5860b287c477ef02f4316ccbc1c74dc834eb8
Vince4130 <vinceblanca@gmail.com>
2024-06-12 11:09:10 +0200

    routes bug resolved

--- resources/views/authors/index.blade.php
+++ resources/views/authors/index.blade.php
@@ -5,17 +5,21 @@
 @section('content')
     <table>
         <tr>
-            <th>#</th>
+            {{-- <th>#</th> --}}
             <th>Lastname</th>
             <th>Firstname</th>
             <th>Birthdate</th>
+            <th>Show</th>
+            <th>Delete</th>
         </tr>
         <?php foreach ($authors as $author) : ?>
         <tr>
-            <td><a href='{{ route('authors.show', $author) }}'>show</a></td>
+            {{-- <td><?= $author->id ?></td> --}}
             <td><?= $author->lastname ?></td>
             <td><?= $author->firstname ?></td>
             <td><?= $author->birth ?></td>
+            <th><a href='{{ route('authors.show', $author) }}'>Details</a></th>
+            <th><a href='{{ route('authors.destroy', $author) }}'>Trash</a></th>
         </tr>
         <?php endforeach; ?>
     </table>
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,21 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
+use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::prefix('/blog')->group(function() {
+Route::prefix('/blog')->group(function () {
     Route::get('/', [AuthorController::class, 'home'])->name('authors.home');
-    Route::get('authors', [AuthorController::class, 'index'])->name('authors.index');
-    Route::get('authors/{author}', [AuthorController::class, 'show'])->name('authors.show');
-    Route::get('authors/create', [AuthorController::class, 'create'])->name('authors.create');
-    Route::post('authors',[AuthorController::class, 'store'])->name('authors.store');
-    Route::get('authors/{author}/edit',[AuthorController::class, 'edit'])->name('authors.edit');
-    Route::post('authors/{author}',[AuthorController::class, 'update'])->name('authors.update');
-    // Route::get('authors/{author}',[AuthorController::class, 'destroy'])->name('authors.destroy');
+    Route::prefix('/authors')->group(function () {
+        Route::get('/index', [AuthorController::class, 'index'])->name('authors.index');
+        Route::get('/show/{author}', [AuthorController::class, 'show'])->name('authors.show');
+        Route::get('/create', [AuthorController::class, 'create'])->name('authors.create');
+        Route::post('/store', [AuthorController::class, 'store'])->name('authors.store');
+        Route::get('/{author}/edit', [AuthorController::class, 'edit'])->name('authors.edit');
+        Route::post('/update/{author}', [AuthorController::class, 'update'])->name('authors.update');
+        Route::get('/delete/{author}', [AuthorController::class, 'destroy'])->name('authors.destroy');
+    });
 });
-
