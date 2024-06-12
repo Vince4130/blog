e7a6972328f88c715760fc30e86a2980ef88accf
Vince4130 <vinceblanca@gmail.com>
2024-06-12 12:47:52 +0200

    sass and vite

--- resources/views/authors/show.blade.php
+++ resources/views/authors/show.blade.php
@@ -4,20 +4,24 @@
 
 @section('content')
     <table>
-        <tr>
-            <th>#</th>
-            <th>Lastname</th>
-            <th>Firstname</th>
-            <th>Birthdate</th>
-        </tr>
-        <tr>
-            <td><?= $author->id ?></td>
-            <td><?= $author->lastname ?></td>
-            <td><?= $author->firstname ?></td>
-            <td><?= $author->birth ?></td>
-        </tr>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Lastname</th>
+                <th>Firstname</th>
+                <th>Birthdate</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><?= $author->id ?></td>
+                <td><?= $author->lastname ?></td>
+                <td><?= $author->firstname ?></td>
+                <td><?= $author->birth ?></td>
+            </tr>
+        </tbody>
     </table>
-        <button type="button" onclick=window.location.href="{{ route('authors.edit', $author) }}">Edit</button>
-        <button type="button" onclick=window.location.href="{{ route('authors.index') }}">Back</button>
- 
+    <a class="navbar-brand" href="{{ route('authors.edit', $author) }}"><i class="fa-solid fa-pen-to-square fa-lg"></i></a>
+    <a class="navbar-brand" href="{{ route('authors.index') }}"><i class="fas fa-home"></i></a>
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,12 @@
-<html>
+<!DOCTYPE html>
+<html lang='fr'>
 
 <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>@yield('title')</title>
+    @vite(['resources/css/app.css', 'resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
 <body>
