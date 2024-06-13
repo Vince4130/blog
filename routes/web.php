0f0879f33f1ed7d9af992c89f025666a9592e56e
Vince4130 <vinceblanca@gmail.com>
2024-06-13 17:47:26 +0200

    Add routes for articles

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -8,7 +9,9 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::prefix('/blog')->group(function () {
-    Route::get('/', [AuthorController::class, 'home'])->name('authors.home');
+    
+    Route::get('/', [AuthorController::class, 'home'])->name('home');
+    
     Route::prefix('/authors')->group(function () {
         Route::get('/index', [AuthorController::class, 'index'])->name('authors.index');
         Route::get('/show/{author}', [AuthorController::class, 'show'])->name('authors.show');
@@ -17,5 +20,15 @@ Route::prefix('/blog')->group(function () {
         Route::get('/{author}/edit', [AuthorController::class, 'edit'])->name('authors.edit');
         Route::post('/update/{author}', [AuthorController::class, 'update'])->name('authors.update');
         Route::get('/delete/{author}', [AuthorController::class, 'destroy'])->name('authors.destroy');
+    });
+
+    Route::prefix('/articles')->group(function () {
+        Route::get('/index', [ArticleController::class, 'index'])->name('articles.index');
+        Route::get('/show/{article}', [ArticleController::class, 'show'])->name('articles.show');
+        Route::get('/create', [ArticleController::class, 'create'])->name('articles.create');
+        Route::post('/store', [ArticleController::class, 'store'])->name('articles.store');
+        Route::get('/{article}/edit', [ArticleController::class, 'edit'])->name('articles.edit');
+        Route::post('/update/{article}', [ArticleController::class, 'update'])->name('articles.update');
+        Route::get('/delete/{article}', [ArticleController::class, 'destroy'])->name('articles.destroy');
     });
 });
