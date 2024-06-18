2c73cb88ccc51aa1d657bfdf8357bc888c18dbf8
Vince4130 <vinceblanca@gmail.com>
2024-06-18 15:00:32 +0200

    Add articles in author show blade

--- resources/views/authors/show.blade.php
+++ resources/views/authors/show.blade.php
@@ -33,6 +33,7 @@
                                 <i class="fa-solid fa-envelope fa-lg"></i>
                             </a>
                         </td>
+                        <td></td>
                     </tr>
                 </tbody>
             </table>
@@ -41,6 +42,44 @@
     <div class="mb-3 list__submit">
         <a class="btn btn-dark" href="{{ route('articles.create', ['author' => $author]) }}"><i class="fa-solid fa-folder-plus fa-lg p-2"></i>Add an article</a>
         <a class="btn btn-dark" href="{{ route('authors.index') }}"><i class="fa-solid fa-rotate-left white p-2"></i>Back</a>
+    </div>
+    <div class="card">
+        <div class="card-body bg-light">
+            <h4>Articles</h4>
+            @if(count($articlesAuthor) > 0)
+            
+            @foreach($articlesAuthor as $article)
+            <div class="accordion p-2" id="accordion{{ $article->id }}">
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{ $article->id }}" aria-expanded="false" aria-controls="collapse{{ $article->id }}">
+                            Title : {{ $article->title }} 
+                        </button>
+                    </h2>
+                    <div id="collapse{{ $article->id }}" class="accordion-collapse collapse" data-bs-parent="#accordion{{ $article->id }}">
+                        <div class="accordion-body">
+                            <div class="content">
+                                <strong>Content : </strong> 
+                                {!! nl2br(e($article->content)) !!}
+                            </div>
+                            <div class="author">
+                                <strong>Author : </strong> 
+                                {{ $author->firstname.' '.$author->lastname }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+            <div>
+                {{ $articlesAuthor->links() }}
+            </div>
+            @else
+                <div class="mb-3">
+                    <h4>There are no published articles for {{ $author->firstname.' '.$author->lastname }}</h4>
+                </div>
+            @endif
+        </div>
     </div>
 </div>
 @endsection
