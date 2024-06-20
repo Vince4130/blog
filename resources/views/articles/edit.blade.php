433baed0ea414faa6a5d8783d00053accf9b5815
Vince4130 <vinceblanca@gmail.com>
2024-06-20 16:19:54 +0200

    Add edit article blade

---
+++ resources/views/articles/edit.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('title', 'Update your Article')
+
+@section('content')
+
+<div class="container">
+    <div class="card">
+        <div class="card-body bg-light">
+            <h1 class="container__title">Update your Article</h1>
+            <form action="{{ route('articles.update', $article) }}" id="myForm" name="myForm" class="myForm" method="POST">
+                @csrf
+                <div class="mb-3">
+                    <label for="title" class="form-label">Title :</label>
+                    <input type="text" class="form-control" id="title" name="title" value="{{ $article->title }}">
+                </div>
+
+                <div class="mb-3">
+                    <label for="content" class="form-label">Content :</label>
+                    <textarea class="form-control " id="content" name="content" rows="5">{{ $article->content }}</textarea>
+                </div>
+
+                <div class="mb-3 container__link">
+                    <button type="submit" class="btn btn-dark"><i class="fa-solid fa-database p-2"></i>Save</button>
+                    <button type="reset" class="btn btn-dark"><i class="fa-solid fa-rotate p-2"></i>Reset</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
